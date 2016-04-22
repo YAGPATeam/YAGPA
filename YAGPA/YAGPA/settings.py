717fe59a4dff6b67df164987ff91c44fbd8ce24c
PapageiSquirrel <raishinshi@hotmail.com>
2016-04-22 17:07:23 +0200

    django-angular
    modif des settings

--- YAGPA/YAGPA/settings.py
+++ YAGPA/YAGPA/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+	'djng',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -94,3 +95,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = '/var/static/angular-example/'
