a8b1395be5888fb225cdbb3c78e4829123f99d35
PapageiSquirrel <raishinshi@hotmail.com>
2016-06-14 18:49:36 +0200

    test

--- YAGPA/static/scripts/controllers.js
+++ YAGPA/static/scripts/controllers.js
@@ -1,4 +1,17 @@
 yagpa.controller('baseCtrl', ['$scope', '$http', function($scope, $http) {
-	
+	$scope.submit = function ($event) {
+        $event.preventDefault();
+        
+        $http({
+            url: '/new',
+            method: 'POST',
+            data: in_data 
+        }).success(function(out_data) {
+            $scope.response = out_data;
+        });
+	};
 }]);
-	
+
+yagpa.controller('playerCtrl', ['$scope', '$http', function($scope, $http) {
+	$scope.player_list = [];
+}]);
