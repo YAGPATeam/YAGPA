24c146fa0e71f109d06a3781d07485a349cb1f98
PapageiSquirrel <raishinshi@hotmail.com>
2016-06-15 18:52:26 +0200

    ajout angular cookies

--- YAGPA/static/scripts/controllers.js
+++ YAGPA/static/scripts/controllers.js
@@ -25,5 +25,12 @@ yagpa.controller('baseCtrl', ['$scope', '$http', '$cookies', function($scope, $h
 }]);
 
 yagpa.controller('playerCtrl', ['$scope', '$http', function($scope, $http) {
-	$scope.player_list = [];
+	$scope.data = [{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'','ffgLicenceStatus':'','firstName':'Damien','grade':'13K','name':'le_Goff','participating':'11111111111111111111','rank':'13K','rating':'800','ratingOrigin':'INI','registeringStatus':'FIN','smmsCorrection':'0'},
+		{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'0634006','ffgLicenceStatus':'L','firstName':'Alexis','grade':'1K','name':'Thaumoux','participating':'11111111111111111111','rank':'1K','rating':'1985','ratingOrigin':'FFG','registeringStatus':'FIN','smmsCorrection':'0'},
+		{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'1300090','ffgLicenceStatus':'L','firstName':'Ronan','grade':'8K','name':'Delasnerie','participating':'11111111111111111111','rank':'8K','rating':'1318','ratingOrigin':'FFG','registeringStatus':'FIN','smmsCorrection':'0'},
+		{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'1200322','ffgLicenceStatus':'L','firstName':'Benjamin','grade':'9K','name':'Saulnier','participating':'11111111111111111111','rank':'9K','rating':'1171','ratingOrigin':'FFG','registeringStatus':'FIN','smmsCorrection':'0'},
+		{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'0534001','ffgLicenceStatus':'L','firstName':'Maël','grade':'1D','name':'Rabasse','participating':'11111111111111111111','rank':'1D','rating':'2123','ratingOrigin':'FFG','registeringStatus':'FIN','smmsCorrection':'0'},
+		{'agaExpirationDate': '','agaId':'','club':'56Lo','country':'FR','egfPin':'','ffgLicence':'0634001','ffgLicenceStatus':'L','firstName':'Mathieu','grade':'3D','name':'Delli-Zotti','participating':'11111111111111111111','rank':'3D','rating':'2318','ratingOrigin':'FFG','registeringStatus':'FIN','smmsCorrection':'0'}];
+	
+	$scope.list_players = $scope.data;
 }]);
---
+++ YAGPA/static/scripts/services.js
@@ -0,0 +1,6 @@
+yagpa.factory('playerService', function() {
+	var player_selected = {};
+	var players_list = [];
+	
+	var loadlist = function() {};
+});
