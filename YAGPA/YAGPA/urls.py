42ed0ae13d124a23859fe77169ea4ff86669ea7b
Kicyfroth <andy.floch@gmail.com>
2016-06-15 21:41:44 +0200

    Urls and sample view for player manager

--- YAGPA/YAGPA/urls.py
+++ YAGPA/YAGPA/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^management/', include('management.urls')),
     url(r'^auth/', include('user_auth.urls')),
+    url(r'^tournament/', include('tournament.urls')),
 ]
--- YAGPA/tournament/views.py
+++ YAGPA/tournament/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
-from django.views.generic.base import View
+from django.views.generic.base import TemplateView
 
 # Create your views here.
-class player_manager(View):
-    def get(self, request, *args, **kwargs):
-        return render(self.request(), "tournament.thml" )
+class player_manager(TemplateView):
+    
+    template_name = "tournament/tournament.html"
+    
+    '''def get(self, request, *args, **kwargs):
+        context = {'tournament_id' : self.kwargs['tournament_id']}
+        return render(request(), , context)
+    '''
+    def get_context_data(self, **kwargs):
+        context = super(player_manager, self).get_context_data(**kwargs)
+        context['tournament_id'] = self.kwargs['tournament_id']
+        return context
