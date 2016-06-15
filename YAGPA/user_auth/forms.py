dcfddc96206712d5e71b8876d7d93ee55b001e30
Kicyfroth <andy.floch@gmail.com>
2016-06-15 14:09:55 +0200

    Added register form

--- YAGPA/user_auth/forms.py
+++ YAGPA/user_auth/forms.py
@@ -6,11 +6,13 @@ Created on 22 avr. 2016
 
 from django import forms
 from django.utils import six
-from djng.forms import NgDeclarativeFieldsMetaclass, NgFormValidationMixin
+from djng.forms import NgDeclarativeFieldsMetaclass, NgFormValidationMixin, NgModelFormMixin, NgForm
 
 class f_login(six.with_metaclass(NgDeclarativeFieldsMetaclass, NgFormValidationMixin, forms.Form)):
     user_name = forms.CharField(label="user name", max_length=255)
     password = forms.PasswordInput("password")
 
     
-    
+class f_register(NgModelFormMixin, NgForm):
+    user_name = forms.CharField(label="user name", max_length=255)
+    user_email = forms.EmailField(label="email")
